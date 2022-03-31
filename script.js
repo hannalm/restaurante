ef7ad58898a98e8186ff3b8d18c98f30b834caee
Hanna Machado <hanna.machado@gmail.com>
2022-03-31 12:36:05 -0300

    modal

--- script.js
+++ script.js
@@ -8,14 +8,15 @@ var inputEmail = document.getElementById('inputEmail')
 var inputQtd = document.getElementById('inputQtd')
 
 function autenticar(event){
-  
+
   event.preventDefault()
 
-  
-  
+  if(inputUsuario.value == 'root' && inputSenha.value == '1234'){
     myTextModal.className = 'text-success'
     myTextModal.innerText = 'Reserva enviada!!!'
-   
+  }
+
   myButtonModal.click()
+  
 }
 
